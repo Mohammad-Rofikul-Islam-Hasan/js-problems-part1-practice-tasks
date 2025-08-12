955ddf097f35bd72cd829a6666df9f32d09d93d2
Mohammad-Rofikul-Islam-Hasan <rofikulislam.ph@gmail.com>
2025-08-13 04:33:49 +0600

    Task-2 Solution Added

--- js-problems-part1-practice-tasks-solutions.js
+++ js-problems-part1-practice-tasks-solutions.js
@@ -4,3 +4,18 @@ function celsiusToFahrenheit(num) {
 }
 
 console.log(celsiusToFahrenheit(2));
+
+// Task 2 Solution
+
+function countDuplicates(arr, num) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+    if (arr[i] === num) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countDuplicates([30, 15, 8, 95, 7, 25, 15], 15));
